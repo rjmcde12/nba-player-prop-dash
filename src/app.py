76ae3badaa513d37452f756db92c296633df7018
Ryan McDermott Welch <ryan@Ryans-MacBook-Air.local>
2024-03-09 14:03:00 -0500

    first commit on  new dev branch for test rollout

--- src/app.py
+++ src/app.py
@@ -18,20 +18,60 @@ import numpy as np
 import nba_api_functions as nba
 import nba_prop_functions as nbaprop
 from dash import Dash, dcc, html, Input, Output, State, Patch, MATCH, ALLSMALLER, callback, dash_table
-import pandas as pd
 import dash_bootstrap_components as dbc
+import pandas as pd
 import plotly.express as px
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
-
 
 
 all_players_df = pd.read_csv('player_gamelogs_2023.csv', index_col=None)
 game_df = pd.read_csv('team_gamelogs_2023.csv', index_col=None)
 
+# +
+default_table_style = {
+    'overflowX': 'auto',
+    'border': '1px solid #dee2e6',
+    'borderCollapse': 'collapse',
+    'width': '100%',
+    'marginBottom': '0'
+}
+
+default_header_style = {
+    'backgroundColor': '#f8f9fa',
+    'fontWeight': 'bold',
+    'border': '1px solid #dee2e6',
+    'textAlign': 'center'
+}
+
+default_cell_style = {
+    'textAlign': 'left',
+    'padding': '8px',
+    'border': '1px solid #dee2e6'
+}
+
+default_conditional_style = [
+    {
+        'if': {'row_index': 'odd'},
+        'backgroundColor': 'rgba(248, 248, 248, 0.8)'
+    },
+    {
+        'if': {'row_index': 'even'},
+        'backgroundColor': 'rgba(255, 255, 255, 0.8)'
+    }
+]
+
+default_styles = {
+    'style_table': default_table_style,
+    'style_header': default_header_style,
+    'style_cell': default_cell_style,
+    'style_data_conditional': default_conditional_style
+}
+
+
+# +
 
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc_css])
-
 server = app.server
 
 app.layout = html.Div(className='dbc', children=[
@@ -55,54 +95,73 @@ app.layout = html.Div(className='dbc', children=[
                     style={'width':'400px'}
                 ),
                 html.Div('Total for the prop:'),
-                dcc.Input(id='prop-line', type='number', min=0, step=0.5, value=1.5, style={'width':'100px'}),
-                dcc.RadioItems(
+                dbc.Input(id='prop-line', type='number', min=0, step=0.5, value=1.5, style={'width':'100px'}),
+                dbc.RadioItems(
                     options=[{'label':'Over', 'value':'Over'},{'label':'Under', 'value':'Under'}],
                     value='Over', id='prop-side', style={'width':'auto', 'align':'left'}
                 )
                 ], gap=2),
         # Rest 
         html.Div(''),
-        html.Button('Generate Data', id='generate-table', n_clicks=0, style={'margin-bottom':'25px', 'margin-top':'10px'}),
-        html.H4(id='b2b-warning', style={'background-color':'yellow'}),
-        html.Div(id='player-selected'), 
+        html.Div(
+            dbc.Button(
+                'Generate Data', id='generate-table', class_name='me-1', n_clicks=0, style={'margin-bottom':'5px', 'margin-top':'10px'})
+        ),
+        html.Div(id='b2b-warning'),
+        html.Hr(), 
+        dbc.Row([
+            dbc.Col([  
+                html.H3('Past Prop Results'),
+                html.Div(id='stat-overview-table')
+            ], width=6
+                   ),
+            dbc.Col([  
+                html.H3('Player Averages'),
+                html.Div(id='averages-table')
+            ], width=6
+                   )
+        ]),
         html.Hr(),
-        html.Div(id='stat-overview-table'),
-        html.H3('Player Averages'),
-        html.Div(id='averages-table'),
-        html.H2('Player Gamelogs'),
-        html.H3(id = 'next-opp-header'),
-        html.Div(id='next-opp-gamelogs'),
-        html.H3('Recent Games'),
-        dcc.RadioItems(options=[
-            {'label':'Last 5 games', 'value':'last_5'},
-            {'label':'Last 10 games', 'value':'last_10'},
-            {'label':'Full Season', 'value':'season'},
-            {'label':'Back to Backs', 'value':'b2b'}
-        ], 
-                       value='last_5', id='radio-items'),
-        html.Div(id='player-stats-table'),
+        html.H3('Player Gamelogs',style={'margin-bottom':'5px', 'margin-top':'10px'}),
+        html.H5(id = 'next-opp-header'),
+        html.Div(id='next-opp-gamelogs', style={'margin-bottom':'10px', 'margin-top':'5px'}),
+        html.H5('Recent Games'),
+        html.Div(
+            [
+                dbc.Tabs(
+                    [
+                        dbc.Tab(label='Last 5', tab_id='last_5'),
+                        dbc.Tab(label='Last 10', tab_id='last_10'),
+                        dbc.Tab(label='Season', tab_id='season'),
+                        dbc.Tab(label='B2B', tab_id='b2b')
+                    ],
+                    id='tab-chosen',
+                    active_tab='last_5'
+                ),
+        html.Div(id='player-stats-table-2'),
+            ]
+        ),
         dcc.Graph(id='prop-graph')
     ])
 ])
 
 @app.callback(
     [
-        Output('player-selected', 'children'),
         Output('b2b-warning', 'children'),
         Output('stat-overview-table', 'children'),
         Output('averages-table', 'children'),
         Output('next-opp-header', 'children'),
         Output('next-opp-gamelogs', 'children'),
-        Output('player-stats-table','children'),
+        Output('player-stats-table-2','children'),
         Output('prop-graph','figure')
     ],
     
     [
         Input('generate-table','n_clicks'),
-        Input('radio-items', 'value')
+        Input('tab-chosen', 'active_tab')
     ],
     [
+        
         State('player-selection', 'value'),
         State('prop-selection', 'value'),
         State('prop-line', 'value'),
@@ -111,8 +170,7 @@ app.layout = html.Div(className='dbc', children=[
 )
 
 
-
-def create_table(n, selected_df, player_selected, prop_selection, prop_line, prop_side):
+def create_table(n, selected_tab, player_selected, prop_selection, prop_line, prop_side):
     
     player_df = nbaprop.player_gamelog_name(all_players_df, player_selected)
     player_df = nbaprop.add_b2b_flag(player_df)
@@ -131,22 +189,12 @@ def create_table(n, selected_df, player_selected, prop_selection, prop_line, pro
         
     last_5, last_10, season, b2b = nbaprop.player_gamelogs_dfs(player_df)
     prop_results = nbaprop.past_prop_results(last_5, last_10, season, b2b, combo_prop, prop_line, prop_side)
-    
-    if selected_df == 'last_5':
-        df = last_5
-    elif selected_df == 'last_10':
-        df = last_10
-    elif selected_df == 'b2b':
-        df = b2b
-    else:
-        df = season
-    
-    b2b_warning = nbaprop.coming_off_b2b(player_id, df)
-    df = nbaprop.create_final_table(df)
+
+    b2b_warning = nbaprop.coming_off_b2b(player_id, season)
     opp_df = nbaprop.create_final_table(opp_df)
     
     if b2b_warning == 'Yes':
-        b2b_message = f'Heads up: {player_selected} is coming off a back-to-back.'
+        b2b_message = dbc.Alert(f'Heads up: {player_selected} is coming off a back-to-back.', color='danger', style={'width':'auto'})
     else:
         b2b_message = ''
 
@@ -155,37 +203,45 @@ def create_table(n, selected_df, player_selected, prop_selection, prop_line, pro
 
     prop_results, avg_table = nbaprop.drop_b2b_row(prop_results, avg_table, b2b_warning)
 
-    prop_results_table = dash_table.DataTable(
-        id='created-stat-overview-table',
-        columns = [{'name':col, 'id':col} for col in prop_results.columns],
-        data = prop_results.to_dict('records'))
+    prop_results_table = dbc.Table.from_dataframe(prop_results, striped=True, bordered=True, hover=True, style={'width':'auto'})
 
-    avg_table = dash_table.DataTable(
-        id='created-avgerages-table',
-        columns = [{'name':col, 'id':col} for col in avg_table.columns],
-        data = avg_table.to_dict('records'))
+    avg_table = dbc.Table.from_dataframe(avg_table, striped=True, bordered=True, hover=True, style={'width':'auto'})
     
     if len(opp_df) == 0:
         opp_table = (f'{player_selected} has not played against {next_opp} yet this season.')
     else:    
-        opp_table = dash_table.DataTable(
-            id='next-opp-gamelogs-table',
-            columns = [{'name':col, 'id':col} for col in opp_df.columns],
-            data = opp_df.to_dict('records'))
+        opp_table = dbc.Table.from_dataframe(opp_df, striped=True, bordered=True, hover=True, style={'width':'auto'})
+
+    if selected_tab == 'last_5':
+        df = last_5
+    elif selected_tab == 'last_10':
+        df = last_10
+    elif selected_tab == 'season':
+        df = season
+    elif selected_tab == 'b2b':
+        df = b2b
+
+    tab_log_df = nbaprop.create_final_table(df)
     
-    gamelogs_table = dash_table.DataTable(
+    games_in_graph = len(tab_log_df)
+    x_tickvals = tab_log_df['Game #'].tolist()
+    step = max(len(x_tickvals) // 10, 1)
+    x_tickvals = x_tickvals[::step]
+    fig = px.line(tab_log_df, x='Game #', y=combo_prop, title=f'Past Results for {combo_prop}', markers=True)
+    fig.update_xaxes(tickvals=x_tickvals, ticktext=[str(val) for val in x_tickvals])
+    fig.add_hline(y=prop_line, line_dash='dash', line_color='red', annotation_text=f'Line {prop_line}', annotation_position='left')
+
+    
+
+    gamelogs_tab = dash_table.DataTable(
         id='player-stats',
-        columns = [{'name':col, 'id':col} for col in df.columns],
-        data = df.to_dict('records'),
-        page_size=10)
+        columns = [{'name':col, 'id':col} for col in tab_log_df.columns],
+        data = tab_log_df.to_dict('records'),
+        page_size=10,
+        **default_styles
+    )
 
-    games_in_graph = len(df)
-    x_values = list(range(1, games_in_graph + 1))
-    fig = px.line(df, x=x_values, y=combo_prop, title=f'Past Results for {combo_prop}', markers=True)
-    fig.add_hline(y=prop_line, line_dash='dash', line_color='red')
-    
-
-    return player_string, b2b_message, prop_results_table, avg_table, opp_header, opp_table, gamelogs_table, fig
+    return  b2b_message, prop_results_table, avg_table, opp_header, opp_table, gamelogs_tab, fig
 
 
 if __name__ == '__main__':
